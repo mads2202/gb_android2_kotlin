fa70d68da2f778eddaf4d4d4264f27c5589ea3d5
dmalyshev <dmitry.malyshev@orioninc.com>
2021-06-22 10:52:12 +0300

    hw 3 done. Most of this work was done in hw2. I really don`t understand how to do that before we`ll able to load data, for displaying it.

--- app/src/main/java/com/mads2202/gb_android2_kotlin/MovieListFragment.kt
+++ app/src/main/java/com/mads2202/gb_android2_kotlin/MovieListFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mads2202.gb_android2_kotlin.model.ActorsLab
+import com.mads2202.gb_android2_kotlin.model.Movie
 
 class MovieListFragment:Fragment() {
     override fun onCreateView(
@@ -17,8 +19,11 @@ class MovieListFragment:Fragment() {
     ): View? {
         val view=inflater.inflate(R.layout.list_fragment,container,false)
         val  recyclerView=view.findViewById<RecyclerView>(R.id.movie_recycler_view)
-        recyclerView.layoutManager=GridLayoutManager(requireContext(),2)
-        recyclerView.adapter=MovieAdapter()
+        recyclerView.layoutManager=LinearLayoutManager(requireContext())
+        recyclerView.adapter=MovieAdapter(arrayListOf(movie1, movie2, movie3))
+        val  recyclerView1=view.findViewById<RecyclerView>(R.id.upcoming_movie_recycler_view)
+        recyclerView1.layoutManager=LinearLayoutManager(requireContext())
+        recyclerView1.adapter=MovieAdapter()
 
     }
 }
