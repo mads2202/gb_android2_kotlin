73bd650d42ac28955e4aace5d90b972ca6554967
dmalyshev <dmitry.malyshev@orioninc.com>
2021-06-25 14:49:26 +0300

    hw 4 done

--- app/src/main/java/com/mads2202/gb_android2_kotlin/MainActivity.kt
+++ app/src/main/java/com/mads2202/gb_android2_kotlin/MainActivity.kt
@@ -2,8 +2,10 @@ package com.mads2202.gb_android2_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
 import com.mads2202.gb_android2_kotlin.model.ActorsLab
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            //тренировка дз4
+        fun View.createAndShow(text: String, actionText: String, action: (View) -> Unit, length: Int = Snackbar.LENGTH_INDEFINITE) {
+            Snackbar.make(this, text, length).setAction(actionText, action).show()}
+
 
     }
 }
